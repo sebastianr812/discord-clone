b313e0cbe194818be69c95a969cc7565e84993eb
Sebastian Rojas <sebastianrojas812@gmail.com>
2023-09-26 14:36:19 -0400

    changes

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,15 +2,15 @@ import NavigationSidebar from "@/components/navigation/NavigationSidebar";
 
 const MainLayout = async ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className="h-full">
+            <div className="h-full">
             <div className="hidden md:flex h-full w-[72px] z-30 flex-col inset-y-0 fixed">
-                <NavigationSidebar />
+            <NavigationSidebar />
             </div>
             <main className="md:pl-[72px] h-full ">
-                {children}
+            {children}
             </main>
-        </div>
-    );
+            </div>
+           );
 }
 
 export default MainLayout;
